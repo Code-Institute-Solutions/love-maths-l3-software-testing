3439bba98277d20a7a0f72cd62332e36840d3cd6
Yoni Lavi <yoni@codeinstitute.net>
2022-04-26 01:19:37 +0000

    A significant refactoring for L3 Software testing
    Refactored to use an ES6 class
    
    Intentionally added an exploit allowing users to change the score

--- 05-tidying-up/01-a-few-last-things/assets/js/script.js
+++ 05-tidying-up/01-a-few-last-things/assets/js/script.js
@@ -1,141 +1,97 @@
+// This constant defines the math operations we support
+// TODO: Add support for division
+const OPERATIONS = {
+    "addition": {"symbol": "+", "calculate": (a, b) => a + b},
+    "subtraction": {"symbol": "-", "calculate": (a, b) => a - b},
+    "multiplication": {"symbol": "x", "calculate": (a, b) => a * b},
+};
+
+/**
+ * Generate a pseudorandom integer between two values, inclusive
+ */
+ function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * Increment the score or incorrect counter in the DOM by 1
+ * NOTE: This functionality is intentionally left outside of the GameEngine, to allow for an example exploit
+ */
+function incrementCounter(elemID) {
+    document.getElementById(elemID).value++;
+}
+
+/**
+ * The main class, which handles the game logic
+ */
+class GameEngine {
+    #operation = "addition"; // The current game operation
+    #operand1; // The first operand for the current question
+    #operand2; // The second operand for the current question
+    #correctAnswer;  // The true answer for the current question
+
+    get operation() {
+        return this.#operation;
+    }
+    set operation(op) {
+        if (OPERATIONS[op]) {
+            this.#operation = op;
+            this.newQuestion();
+        } else {
+            alert(`Unimplemented operation: ${op}, staying with ${this.#operation}`);
+        }
+    }
+
+    // Generate a new question and update the DOM
+    newQuestion() {
+        document.getElementById("answer-box").value = "";
+        document.getElementById("answer-box").focus();
+    
+        this.#operand1 = randomInteger(0, 25);
+        this.#operand2 = randomInteger(0, 25);
+        this.#correctAnswer = OPERATIONS[this.#operation].calculate(this.#operand1, this.#operand2);
+
+        document.getElementById('operand1').textContent = this.#operand1;
+        document.getElementById('operand2').textContent = this.#operand2;
+        document.getElementById('operator').textContent = OPERATIONS[this.#operation].symbol;
+    }
+
+    // Check whether the user's answer is correct, update the counters and proceed to the next question
+    checkAnswer() {
+        let userAnswer = parseInt(document.getElementById("answer-box").value);
+
+        if (userAnswer === this.#correctAnswer) {
+            alert("Hey! You got it right! :D");
+            incrementCounter("score");
+        } else {
+            alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${this.#correctAnswer}!`);
+            incrementCounter("incorrect");
+        }
+
+        this.newQuestion()
+    }
+}
+
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
-
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+    let game = new GameEngine();
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                checkAnswer();
-            } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
-            }
-        });
+    for (let button of document.getElementsByTagName("button")) {
+        if (button.dataset.operation === "submit") {
+            button.addEventListener("click", game.checkAnswer);
+        } else {
+            button.addEventListener("click", function() {
+                game.operation = this.dataset.operation;
+            });
+        }
     }
 
     document.getElementById("answer-box").addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
-            checkAnswer();
+            game.checkAnswer();
         }
     });
 
-    runGame("addition");
-
+    game.newQuestion();
 });
-
-/**
- * The main game "loop", called when the script is first loaded
- * and after the user's answer has been processed
- */
-function runGame(gameType) {
-
-    document.getElementById("answer-box").value = "";
-    document.getElementById("answer-box").focus();
-
-    // Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 25) + 1;
-    let num2 = Math.floor(Math.random() * 25) + 1;
-
-    if (gameType === "addition") {
-        displayAdditionQuestion(num1, num2);
-    } else if (gameType === "multiply") {
-        displayMultiplyQuestion(num1, num2);
-    } else if (gameType === "subtract" ) {
-        displaySubtractQuestion(num1, num2);
-    } else {
-        alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
-    }
-
-}
-
-/**
- * Checks the answer against the first element in
- * the returned calculateCorrectAnswer array
- */
-function checkAnswer() {
-
-    let userAnswer = parseInt(document.getElementById("answer-box").value);
-    let calculatedAnswer = calculateCorrectAnswer();
-    let isCorrect = userAnswer === calculatedAnswer[0];
-
-    if (isCorrect) {
-        alert("Hey! You got it right! :D");
-        incrementScore();
-    } else {
-        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
-        incrementWrongAnswer();
-    }
-
-    runGame(calculatedAnswer[1]);
-
-}
-
-/**
- * Gets the operands (the numbers) and the operator (plus, minus etc)
- * directly from the dom, and returns the correct answer.
- */
-function calculateCorrectAnswer() {
-
-    let operand1 = parseInt(document.getElementById('operand1').innerText);
-    let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById("operator").innerText;
-
-    if (operator === "+") {
-        return [operand1 + operand2, "addition"];
-    } else if (operator === "x") {
-        return [operand1 * operand2, "multiply"];
-    } else if (operator === "-") {
-        return [operand1 - operand2, "subtract"];
-    } else {
-        alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`;
-    }
-
-}
-
-/**
- * Gets the current score from the DOM and increments it by 1
- */
-function incrementScore() {
-
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
-
-}
-
-/**
- * Gets the current tally of incorrect answers from the DOM and increments it by 1
- */
-function incrementWrongAnswer() {
-
-    let oldScore = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
-    
-}
-
-function displayAdditionQuestion(operand1, operand2) {
-
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
-    
-}
-
-function displaySubtractQuestion(operand1, operand2) {
-
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
-    document.getElementById('operator').textContent = "-";
-
-}
-
-function displayMultiplyQuestion(operand1, operand2) {
-
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "x";
-
-}
